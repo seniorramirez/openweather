bf1e84df888537aed0c82e93daed03664cadf76c
kevin ramirez <kevin.ramirez@sigmamovil.com>
2017-04-11 17:28:37 -0500

    inicial

---
+++ app/src/main/java/com/example/kevinramirez/openweatcher/Misc/Connection.java
@@ -0,0 +1,68 @@
+package com.example.kevinramirez.openweatcher.Misc;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by william.montiel on 4/11/2017.
+ */
+
+public class Connection {
+
+    protected final String TAG = "Connection";
+
+    public void endPoint(final String url, final String jsonPost, final Context context,final Boolean isPost) throws Exception{
+        try{
+            RequestQueue requestQueue = Volley.newRequestQueue(context);
+            StringRequest stringRequest = new StringRequest(
+                    (isPost == true)?1:0,
+                    url,
+                    new com.android.volley.Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {}
+                    },
+                    new com.android.volley.Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                        }
+                    }) {
+                @Override
+                public Map<String,String> getParams(){
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("Content-Type", "application/x-www-form-urlencoded");
+                    params.put("data", jsonPost);
+
+                    return params;
+                }            };
+            stringRequest.setRetryPolicy(new RetryPolicy() {
+                @Override
+                public int getCurrentTimeout() {
+                    return 50000;
+                }
+
+                @Override
+                public int getCurrentRetryCount() {
+                    return 50000;
+                }
+
+                @Override
+                public void retry(VolleyError error) throws VolleyError {}
+            });
+
+            requestQueue.add(stringRequest);
+        }catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/kevinramirez/openweatcher/WeatcherFragment.java
@@ -0,0 +1,91 @@
+package com.example.kevinramirez.openweatcher;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class WeatcherFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    Typeface weatherFont;
+
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+
+    Handler handler;
+
+    public WeatcherFragment() {}
+
+    public static WeatcherFragment newInstance(String param1, String param2) {
+        WeatcherFragment fragment = new WeatcherFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_weatcher, container, false);
+        cityField = (TextView)view.findViewById(R.id.city_field);
+        updatedField = (TextView)view.findViewById(R.id.updated_field);
+        detailsField = (TextView)view.findViewById(R.id.details_field);
+        currentTemperatureField = (TextView)view.findViewById(R.id.current_temperature_field);
+        weatherIcon = (TextView)view.findViewById(R.id.weather_icon);
+        weatherIcon.setTypeface(weatherFont);
+
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
